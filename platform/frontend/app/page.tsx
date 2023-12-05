b5fc954768b54f8521b3b1b002526e72b3ebc19d
thisIsDanielJin <danielwodke25@gmail.com>
2023-12-05 20:49:35 +0100

    feat: add footer

--- platform/frontend/app/page.tsx
+++ platform/frontend/app/page.tsx
@@ -1,14 +1,18 @@
 import Header from "./components/Header";
 import FileUpload from "./components/FileUpload";
 import { InfoBox } from "./components/InfoBox";
+
 export default function Home() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-300">
+      <main className="flex flex-1 flex-col items-center justify-center p-24 bg-gray-300">
         <InfoBox />
         <FileUpload />
       </main>
-    </>
+      <footer className="w-full bg-gray-800 text-white text-center p-4">
+        <p>© 2023 A TUB Project - Explainable AI Project.</p>
+      </footer>
+    </div>
   );
 }
