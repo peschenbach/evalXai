d8170559b07d69c0de9de044d818eaf0c3efd69d
thisIsDanielJin <danielwodke25@gmail.com>
2023-12-17 14:25:52 +0100

    feat: file upload implemented

---
+++ platform/frontend/TODO.md
@@ -0,0 +1,17 @@
+Login user management
+kaggle like
+sidebar with competitions (datasets )
+When click on dataset card it opens description
+Leaderboard.
+
+Field for entering the name of XAI method
+Download button for the dataset.
+Xatris datasets
+
+Upload in zip file.
+
+Report Diagram of the api communications
+
+---
+
+red
--- platform/frontend/app/components/FileUpload.tsx
+++ platform/frontend/app/components/FileUpload.tsx
@@ -1,19 +1,95 @@
-import React from "react";
+"use client";
+import React, { useState, ChangeEvent, DragEvent } from "react";
 import Button from "@mui/material/Button";
+import axios from "axios";
 
-export const FileUpload = () => {
+const uploadFile = async (
+  file: File,
+  setUploadStatus: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const response = await axios.post("/api/preds", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    console.log("File successfully uploaded", response.data);
+    setUploadStatus("✅ File successfully uploaded!");
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error("Upload failed", error.response.data);
+      setUploadStatus("❌ File upload was not successful.");
+    } else {
+      console.error("Error during file upload", error);
+      setUploadStatus("❌ File upload was not successful.");
+    }
+  }
+};
+
+export const FileUpload: React.FC = () => {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [uploadStatus, setUploadStatus] = useState<string>("");
+
+  const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null;
+    if (file) {
+      setSelectedFile(file);
+      uploadFile(file, setUploadStatus);
+    }
+  };
+
+  const handleButtonClick = () => {
+    document.getElementById("fileInput")?.click();
+  };
+
+  const handleDrop = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      setSelectedFile(file);
+      uploadFile(file, setUploadStatus);
+    }
+  };
+
+  const handleDragOver = (event: DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <>
       <div className="w-full max-w-3xl mx-auto bg-white shadow-md p-8 rounded-lg">
         <h2 className="text-2xl font-semibold mb-4 text-center">
           Upload your file
         </h2>
-        <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-10 rounded-lg mb-4">
+        <div
+          className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-10 rounded-lg mb-4"
+          onDrop={handleDrop}
+          onDragOver={handleDragOver}
+        >
           <p className="text-gray-500 mb-3">Drag and drop your file here</p>
-          <Button variant="contained" size="large" className="bg-gray-800">
+          <input
+            type="file"
+            id="fileInput"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
+          />
+          <Button
+            variant="contained"
+            size="large"
+            className="bg-gray-800"
+            onClick={handleButtonClick}
+          >
             Browse Files
           </Button>
         </div>
+        {selectedFile && (
+          <p className="text-center ">File selected: {selectedFile.name}</p>
+        )}
+        {uploadStatus && <p className="text-center mt-4">{uploadStatus}</p>}
       </div>
     </>
   );
