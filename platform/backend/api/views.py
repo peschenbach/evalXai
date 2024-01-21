6026bd8b5dd0ad918ac46076b09bc2a7d150cafb
Sami.Hached <samihached2015@gmail.com>
2024-01-21 20:22:28 +0100

    modified backend to receive file and save it inside the worker container. stil struggling with executing it though

--- platform/backend/api/views.py
+++ platform/backend/api/views.py
@@ -1,5 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, parser_classes
 from rest_framework.response import Response
+from rest_framework.parsers import FileUploadParser
 from rest_framework import status
 from .models import *
 from .serializers import *
@@ -7,6 +8,7 @@ from .worker_utils import trigger_evaluation_script_inside_worker
 
 
 @api_view(['GET', 'POST'])
+@parser_classes([FileUploadParser])
 def prediction_list(request):
 
     if request.method == "GET":
@@ -15,14 +17,24 @@ def prediction_list(request):
         return Response(serializer.data)
 
     if request.method == "POST":
-        serializer = PredictionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            # TODO: spawn container and run evaluate.py
-            # before spawning, get the network name as a parameter,
-            # and connect the new container to it.
-            trigger_evaluation_script_inside_worker()
-            return Response(data=serializer.data, status = status.HTTP_201_CREATED)
+        input_file = request.FILES.get('file')
+
+        if input_file is None:
+            return Response({'error': f'error getting the input file'}, status=status.HTTP_400_BAD_REQUEST)
+
+        file_contents = input_file.read().decode('utf-8')
+        message = trigger_evaluation_script_inside_worker(file_contents)
+
+        return Response({'message': message}, status=status.HTTP_200_OK)
+
+        # serializer = PredictionSerializer(data=request.data)
+        # if serializer.is_valid():
+        #     serializer.save()
+        #     # TODO: spawn container and run evaluate.py
+        #     # before spawning, get the network name as a parameter,
+        #     # and connect the new container to it.
+        #     trigger_evaluation_script_inside_worker()
+        #     return Response(data=serializer.data, status = status.HTTP_201_CREATED)
 
 
 @api_view(['GET', 'POST'])
--- platform/backend/api/worker_utils.py
+++ platform/backend/api/worker_utils.py
@@ -1,26 +1,51 @@
 import logging
-
+import io
+import tarfile
 import docker
 
-def trigger_evaluation_script_inside_worker():
-    # logging.DEBUG("entered trigger function")
-    # Create a Docker client
-    client = docker.from_env()
 
-    # Specify the name or ID of the running container
-    container_name_or_id = "evalxai-worker-1"
+def trigger_evaluation_script_inside_worker(file_contents: str):
+    try:
+        # logging.DEBUG("entered trigger function")
+        # Create a Docker client
+        client = docker.from_env()
 
-    # logging.DEBUG("created docker client")
+        # Specify the name or ID of the running container
+        container_name_or_id = "evalxai-worker-1"
+        container = client.containers.get(container_name_or_id)
 
-    # Get the container object
-    container = client.containers.get(container_name_or_id)
+        # Specify the path and filename inside the container
+        destination_path = "/worker"
+        compressed_filename = "uploaded_file.tar"
+        decompressed_filename = "uploaded_file.py"
+        file_path_in_container = f"{destination_path}/{compressed_filename}"
 
-    # logging.DEBUG("successfully got the worker container")
+        file_contents_bytes = file_contents.encode('utf-8')
+
+        # Create a tarball from the bytes
+        tar_data = io.BytesIO()
+        with tarfile.open(fileobj=tar_data, mode='w') as tar:
+            tarinfo = tarfile.TarInfo(name=decompressed_filename)
+            tarinfo.size = len(file_contents_bytes)
+            tar.addfile(tarinfo, io.BytesIO(file_contents_bytes))
+
+        # Copy the tarball into the container
+        tar_data.seek(0)
+        container.put_archive(destination_path, tar_data)
+
+        # return "successfully saved the file"
+    except Exception as e:
+        return f"no file saved. error: {e}"
+
+    command = f"tar -xf {file_path_in_container} -C {destination_path}"
+    container.exec_run(command)
 
     # Execute a command in the container
-    command = "python evaluate.py"
+    command = f"python {destination_path}/{decompressed_filename}"
     result = container.exec_run(command)
 
+    #TODO: read the saved file and send it back in result to see its content
+    return f"Executed input file."
     # logging.DEBUG(result.output.decode("utf-8"))
 
     # Print the output of the command
