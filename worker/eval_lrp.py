084782639da51796368c8b72667be93a9d7455ca
peeb <eschenbach.paul@gmail.com>
2024-02-02 19:31:09 +0100

    eval lrp v3

--- worker/eval_lrp.py
+++ worker/eval_lrp.py
@@ -6,7 +6,7 @@ from emd import *
 
 # load the generated tetris data
 data_file = "linear_1d1p_0.18_uncorrelated"
-data_path = "/data/" + data_file + ".pkl"
+data_path = "./data/" + data_file + ".pkl"
 with open(data_path, 'rb') as file:
     data = pkl.load(file)
 
@@ -47,28 +47,11 @@ def get_lrp_attributions(data: torch.Tensor, target: torch.Tensor, model: torch.
 # alternatively do this for the whole dataset (this can be very slow!!)
 batch_size = 100
 
-# Convert input data to the desired data type
-# Assuming the desired data type is float32
-x_train = data[data_file].x_train.to(torch.float32)
-
-# Reshape your input data to match the expected shape [batch_size, channels, height, width]
-# x_train_reshaped = x_train.view(-1, 1, 8, 8)  # Assuming your data is 8x8
-
-
-# lrp_explanations = get_lrp_attributions(
-#     x_train_reshaped[:batch_size][:batch_size], data[data_file].y_train[:batch_size], model)
-
 lrp_explanations = get_lrp_attributions(
-    x_train[:batch_size].to(t.float).reshape(100,1,8,8), data[data_file].y_train[:batch_size], model)
-
-c = continuous_emd(data[data_file].masks_test[0], lrp_explanations[0].detach().numpy())
-print(c)
-
-# lrp_explanations = get_lrp_attributions(
-#     data[data_file].x_train[:batch_size], data[data_file].y_train[:batch_size], model)
+    data[data_file].x_train[:batch_size].to(torch.float).reshape(100, 1, 8, 8), data[data_file].y_train[:batch_size], model)
 
 # producing the EMD score for LRP explanation of sample zero
 # emd_score = continuous_emd(data[data_file].masks_train[0],
 #                            lrp_explanations[0].detach().numpy())
 
-# print(lrp_explanations)
+print(lrp_explanations)
