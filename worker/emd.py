d74ab3a8d7f55b3626d3cb0cb12a52f20fbb059b
peeb <eschenbach.paul@gmail.com>
2024-02-02 16:40:14 +0100

    eval lrp v02

--- worker/emd.py
+++ worker/emd.py
@@ -2,6 +2,7 @@
 
 from scipy.spatial.distance import cdist
 from scipy.ndimage import gaussian_filter
+import numpy as np
 
 from ot.lp import emd
 
@@ -13,18 +14,23 @@ from ot.lp import emd
 # combined_mask = combined_mask.reshape((8,8))
 
 # euclidean distance cost matrix
+
+
 def create_cost_matrix(edge_length=64):
-    mat = np.indices((edge_length,edge_length))
+    mat = np.indices((edge_length, edge_length))
     coords = []
     for i in range(edge_length):
         for j in range(edge_length):
             coords.append((mat[0][i][j], mat[1][i][j]))
     coords = np.array(coords)
-    return cdist(coords,coords)
+    return cdist(coords, coords)
+
 
 cost_matrix_8by8 = create_cost_matrix(8)
 
 # Scale matrix to sum to 1
+
+
 def sum_to_1(mat):
     return mat / np.sum(mat)
 
@@ -32,11 +38,11 @@ def sum_to_1(mat):
 # Calculate EMD for full, continuous-valued, attribution
 # score = 1-(EMD/Dmax), where Dmax = max euclidean distance, aka sqrt(7^2 + 7^2)=9.8995 for the 8x8 data
 def continuous_emd(gt_mask, attribution, n_dim=64):
-#     cost_matrix = cost_matrix_64by64
-#     if n_dim == 64:
+    #     cost_matrix = cost_matrix_64by64
+    #     if n_dim == 64:
     cost_matrix = cost_matrix_8by8
 
-    _, log = emd(sum_to_1(gt_mask.reshape(n_dim)).astype(np.float64), sum_to_1(np.abs(attribution).reshape(n_dim)).astype(np.float64), cost_matrix, numItermax=200000, log=True)
+    _, log = emd(sum_to_1(gt_mask.reshape(n_dim)).astype(np.float64), sum_to_1(np.abs(
+        attribution).reshape(n_dim)).astype(np.float64), cost_matrix, numItermax=200000, log=True)
 
     return 1 - (log['cost']/np.sqrt(n_dim + n_dim))
-
--- worker/eval_lrp.py
+++ worker/eval_lrp.py
@@ -1,34 +1,34 @@
+import torch
 from captum.attr import GradientShap, Lime, LRP
 import pickle as pkl
 from torch import load
 from emd import *
 
 # load the generated tetris data
-file_name = "linear_1d1p_0.18_uncorrelated"
-path = "./data/" + file_name + ".pkl"
-with open(path, 'rb') as file:
+data_file = "linear_1d1p_0.18_uncorrelated"
+data_path = "./data/" + data_file + ".pkl"
+with open(data_path, 'rb') as file:
     data = pkl.load(file)
 
 # train data of shape (N,64)
-x_train = data[file_name].x_train
+x_train = data[data_file].x_train
 
 # train prediction labels of shape (N)
-y_train = data[file_name].y_train
+# y_train = data[data_file].y_train
 
 # test data
-x_test = data[file_name].x_test
+# x_test = data[data_file].x_test
 
 # test prediction labels
-y_test = data[file_name].y_test
+# y_test = data[data_file].y_test
 
 # train explanation ground truth masks of shape (N, 64)
-masks_train = data[file_name].masks_train.shape
+# masks_train = data[data_file].masks_train.shape
 
-# load the xai model
-file_name = "linear_1d1p_0.18_uncorrelated"
-path = "./xai_model/" + file_name + ".pt"
-model = load(
-    './artifacts/tetris/training/2022-09-01-13-07/linear_0.18_uncorrelated_LLR_0_0.pt')
+# load model
+model_file = "linear_1d1p_0.18_uncorrelated_LLR_1_0"
+model_path = "./ai_model/" + model_file + ".pt"
+model = load(model_path)
 
 # taken from /xai/methods.py
 
@@ -37,15 +37,6 @@ model = load(
 # target: prediction target shape (N)
 # model: trained model .pt object
 # and optionally (here for GradSHAP and LIME, not for LRP)
-# baselines: starting point for explanation computation, can be a tensor of zeroes shape (N,64)
-
-
-def get_gradient_shap_attributions(data: torch.Tensor, target: torch.Tensor, model: torch.nn.Module, baselines: torch.Tensor) -> torch.tensor:
-    return GradientShap(model).attribute(data.float(), target=target, baselines=baselines)
-
-
-def get_lime_attributions(data: torch.Tensor, target: torch.Tensor, model: torch.nn.Module, baselines: torch.Tensor) -> torch.tensor:
-    return Lime(model).attribute(data, target=target, baselines=baselines)
 
 
 def get_lrp_attributions(data: torch.Tensor, target: torch.Tensor, model: torch.nn.Module) -> torch.tensor:
@@ -56,11 +47,25 @@ def get_lrp_attributions(data: torch.Tensor, target: torch.Tensor, model: torch.
 # alternatively do this for the whole dataset (this can be very slow!!)
 batch_size = 100
 
+# Convert input data to the desired data type
+# Assuming the desired data type is float32
+x_train = data[data_file].x_train.to(torch.float32)
+
+# Reshape your input data to match the expected shape [batch_size, channels, height, width]
+# x_train_reshaped = x_train.view(-1, 1, 8, 8)  # Assuming your data is 8x8
+
+
+# lrp_explanations = get_lrp_attributions(
+#     x_train_reshaped[:batch_size][:batch_size], data[data_file].y_train[:batch_size], model)
+
 lrp_explanations = get_lrp_attributions(
-    data['linear_0.18_uncorrelated'].x_train[:batch_size], data['linear_0.18_uncorrelated'].y_train[:batch_size], model)
+    x_train[:batch_size], data[data_file].y_train[:batch_size], model)
+
+# lrp_explanations = get_lrp_attributions(
+#     data[data_file].x_train[:batch_size], data[data_file].y_train[:batch_size], model)
 
 # producing the EMD score for LRP explanation of sample zero
-continuous_emd(data['linear_0.18_uncorrelated'].masks_train[0],
-               lrp_explanations[0].detach().numpy())
+# emd_score = continuous_emd(data[data_file].masks_train[0],
+#                            lrp_explanations[0].detach().numpy())
 
-print(x_train)
+# print(lrp_explanations)
