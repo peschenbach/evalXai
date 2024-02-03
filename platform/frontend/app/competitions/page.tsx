5aaaeca6836a71eb39ef2a910b763a10b79ad53d
danielwodkeTecleadVentures <daniel.jin.wodke@teclead-ventures.de>
2024-02-03 15:53:50 +0100

    feat: new pages and buttons for upload

---
+++ platform/frontend/app/competitions/page.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import Header from "@/app/components/Header";
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import Link from "next/link";
+import tetris from "@/public/tetris.png";
+
+const Competitions = () => {
+  const competitionName = "Tetris";
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex flex-1 flex-col items-center justify-center p-24 bg-gray-300">
+        <h1 className="text-4xl font-bold mb-4">Competitions</h1>
+        <p className="text-lg mb-8">
+          In this competition section you can explore and participate in
+          Explainable AI Benchmarking Challenges.
+        </p>
+        <p className="mb-10">
+          In the following you can see all available competitions. Click on the
+          competition to learn more and participate.
+        </p>
+        <Link href={`/competitions/tetris`} passHref>
+          <Card sx={{ maxWidth: 345, cursor: "pointer" }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                sx={{
+                  height: 230,
+                }}
+                height="140"
+                image={tetris.src}
+                alt="Tetris"
+              />
+
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {competitionName} Competition
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Click here to learn more and participate.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Link>
+      </main>
+      <footer className="w-full bg-gray-800 text-white text-center p-4">
+        <p>© 2023 A TUB Project - Explainable AI Project.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Competitions;
