47ae5202a77e89c0633cc5dde061027286f41c2b
danielwodkeTecleadVentures <daniel.jin.wodke@teclead-ventures.de>
2024-02-06 20:05:47 +0100

    feat: styling fix and upload progress

--- platform/frontend/app/components/FileUpload.tsx
+++ platform/frontend/app/components/FileUpload.tsx
@@ -1,19 +1,71 @@
-import React from "react";
+import React, { useRef, useState, ChangeEvent } from "react";
 import Button from "@mui/material/Button";
+import axios, { AxiosError } from "axios";
 
 export const FileUpload = () => {
+  const [uploadStatus, setUploadStatus] = useState<string>("");
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const selectedFile = files[0];
+      const formData = new FormData();
+      formData.append("file", selectedFile);
+
+      try {
+        const response = await axios.post(
+          "http://localhost:8000/api/xai/1/",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          }
+        );
+        setUploadStatus(
+          `File uploaded successfully: ${JSON.stringify(response.data)}`
+        );
+      } catch (error) {
+        let errorMessage = "An error occurred";
+        if (axios.isAxiosError(error)) {
+          errorMessage = `Error uploading file: ${
+            error.response ? JSON.stringify(error.response.data) : errorMessage
+          }`;
+        }
+        setUploadStatus(errorMessage);
+      }
+    }
+  };
+
+  const handleButtonClick = () => {
+    fileInputRef.current?.click();
+  };
+
   return (
     <>
       <div className="w-full max-w-3xl mx-auto bg-white shadow-md p-8 rounded-lg">
         <h2 className="text-2xl font-semibold mb-4 text-center">
           Upload your file
         </h2>
-        <div className="flex flex-col items-center justify-center border-2 border-dashed border-gray-300 p-10 rounded-lg mb-4">
-          <p className="text-gray-500 mb-3">Drag and drop your file here</p>
-          <Button variant="contained" size="large" className="bg-gray-800">
-            Browse Files
+        <div className="flex justify-center">
+          <input
+            type="file"
+            onChange={handleFileUpload}
+            style={{ display: "none" }}
+            ref={fileInputRef}
+          />
+
+          <Button
+            variant="contained"
+            color="primary"
+            className="bg-blue-500"
+            onClick={handleButtonClick}
+          >
+            Select and Upload File
           </Button>
         </div>
+        {uploadStatus && <p>{uploadStatus}</p>}
       </div>
     </>
   );
